1d83bdb847b4133cbecfdb9b3f1eb9b431a91439
nino248 <nino2481992@gmail.com>
2023-02-21 12:13:21 +0100

    Primer commit

---
+++ src/holaMundo/HolaMundo.java
@@ -0,0 +1,13 @@
+package holaMundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// Imprimimos por consola un mensaje
+		
+			System.out.println(" Hola Mundo ");
+		
+		
+	}
+
+}
